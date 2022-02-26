70824072262334325fe4c0c37a817afc2bba45c7
Brianna Woodruff <briannaewoodruff@gmail.com>
2022-02-25 19:03:41 -0600

    npm packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+// ant.design
+import 'antd/dist/antd.css';
+// font awesome icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCode, faPerson, faMailReply } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faInstagram, faVimeo } from '@fortawesome/free-brands-svg-icons'
+// routing
+import { NavLink, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
