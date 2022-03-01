61c42a0cd72b1de3285e06de27b765f6a3dcbc78
Brianna Woodruff <briannaewoodruff@gmail.com>
2022-02-28 22:08:22 -0600

    resume component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Head from './components/header/Header'
 import AboutMe from './components/about-me/AboutMe'
 import Project from './components/project/Project'
 import ContactMe from './components/contact-me/ContactMe'
+import Resume from './components/resume/Resume'
 import Footer from './components/footer/Footer'
 // ant.design
 import 'antd/dist/antd.css';
@@ -20,7 +21,7 @@ function App() {
         <Route path='/about-me' element={<AboutMe />} />
         <Route path='/portfolio' element={<Project />} />
         <Route path='/contact-me' element={<ContactMe />} />
-        {/* <Route path='/resume' element={<Weather />} /> */}
+        <Route path='/resume' element={<Resume />} />
       </Routes>
       <Footer />
     </main>
---
+++ src/components/resume/Resume.js
@@ -0,0 +1,29 @@
+import './resume.css';
+import ProfileImg from '../../assets/images/headshot2022.jpg'
+import downloadResume from '../../assets/files/Brianna-Woodruff_Resume-2022.pdf'
+
+export default function Resume() {
+    return (
+        <section className="card Resume">
+            {/* <h2 id="resumetitle">Resume</h2> */}
+            <aside>
+                <div className="avatar">
+                    <img src={ProfileImg} height={300} width={300} className="profile-img" alt="profileimg" />
+                </div>
+                <div className="aboutlinks">
+                    <a target="_blank" href="https://github.com/briannawoodruff"> GitHub</a>
+                    <a target="_blank" href="https://linkedin.com/in/briannaewoodruff/"> LinkedIn</a>
+                    <a target="_blank" href="https://vimeo.com/294283512/"> Vimeo Reel</a>
+                </div>
+            </aside>
+
+            <div className="info">
+                <p>
+                    You can download my resume here: 
+                </p>
+                <a id="resumedownload" href={downloadResume} download> Download Resume
+                </a>
+            </div>
+        </section>
+    )
+}
